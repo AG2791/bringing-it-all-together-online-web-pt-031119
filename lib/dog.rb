a89382ee23656514d8b34374adc646b9c793bc6f
AJ Guiles <ashiguiles@gmail.com>
2019-06-15 23:28:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,8 +61,20 @@ def update
    
 end
  
-# def save
+def save
+  
+   if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
    
-# end
+end
   
 end
